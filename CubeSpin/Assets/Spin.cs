d87cac7fffe2cb5e377814a012a83b81780abe03
addshore <addshorewiki@gmail.com>
2014-10-24 16:46:02 +0100

    Add cube movement and simplify code

--- CubeSpin/Assets/Spin.cs
+++ CubeSpin/Assets/Spin.cs
@@ -3,57 +3,49 @@ using System.Collections;
 
 public class Spin : MonoBehaviour {
 
-	string fingerCount;
-	int fingersOnScreen;
+	new GUIText guiText;
 
 	void Start () {
-		guiText.pixelOffset = new Vector2( Screen.width/2, Screen.height/15 );
+		guiText = GameObject.Find("GUI Text").GetComponent<GUIText>();
 	}
 
 	// Update is called once per frame
 	void Update () {
 
-		fingersOnScreen = 0;
+		int fingersOnScreen = 0;
 
 		foreach (Touch touch in Input.touches) {
-
 			if (touch.phase != TouchPhase.Ended && touch.phase != TouchPhase.Canceled){
 
 				fingersOnScreen ++;
+
+				if( touch.position.x < Screen.width/2 ) {
+					//Move left
+					transform.position += Vector3.left * Time.deltaTime;
+				} else {
+					//Move right
+					transform.position += Vector3.right * Time.deltaTime;
+				}
+				if( touch.position.y < Screen.height/2 ) {
+					//Move down
+					transform.position += Vector3.down * Time.deltaTime;
+				} else {
+					//Move up
+					transform.position += Vector3.up * Time.deltaTime;
+				}
 
 			}
 		}
 
 		if (fingersOnScreen > 0){
-
-			guiText.text = "The User has " + fingerCount + " finger(s) touching the screen";
-
-			if (fingersOnScreen == 2){
-
-				guiText.text = "The User has " + fingerCount + " finger(s) touching the screen";
-				transform.Rotate(0, 2f, 0);
-			}
-			if (fingersOnScreen == 3){
-
-				guiText.text = "The User has " + fingerCount + " finger(s) touching the screen";
-				transform.Rotate(0, 3f, 0);
-			}
-
-			if (fingersOnScreen == 4){
-
-				guiText.text = "The User has " + fingerCount + " finger(s) touching the screen";
-				transform.Rotate(0, 4f, 0);
-			}
-		}
-
-		else{
-
+			guiText.text = "The User has " + fingersOnScreen.ToString() + " finger(s) touching the screen";
+			transform.Rotate(0, (float)fingersOnScreen, 0);
+		}else{
 			guiText.text = "Touch me to go faster!";
+			transform.Rotate(0, 1f, 0);
 		}
 
-		transform.Rotate(0, 1f, 0);
 
-		fingerCount = fingersOnScreen.ToString();
 	}
 
 }
