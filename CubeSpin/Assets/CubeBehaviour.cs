3e2efbda54767471c85da84f6121c07da075b05a
addshore <addshorewiki@gmail.com>
2014-11-16 14:32:37 +0000

    Add more functionality to CubeSpin

--- CubeSpin/Assets/CubeBehaviour.cs
+++ CubeSpin/Assets/CubeBehaviour.cs
@@ -6,8 +6,6 @@ public class CubeBehaviour : MonoBehaviour {
 	private GameObject cubeObject;
 	new GUIText guiText;
 	new Camera camera;
-
-	private bool rotating;
 	
 	void Start () {
 		//Get objects
@@ -22,29 +20,37 @@ public class CubeBehaviour : MonoBehaviour {
 	void Defaults () {
 		cubeObject.renderer.material.color = Color.red;
 		cubeObject.transform.position = new Vector3(0,0,0);
-		cubeObject.transform.rotation = new Vector3(0,0,0);
+		cubeObject.transform.rotation = new Quaternion(0,0,0,0);
 		camera.fieldOfView = 60;
 	}
 	
 	void Update () {
-		if (Input.touchCount == 1) {
+		if( Input.touchCount == 0 ) {
+			cubeObject.transform.Rotate( 0, 0f, 0 );
+			cubeObject.transform.rotation = new Quaternion(0,0,0,0);
+		} else if (Input.touchCount == 1) {
 			Touch touchZero = Input.GetTouch(0);
+
 			if (touchZero.phase == TouchPhase.Began) {
 				Ray ray = Camera.main.ScreenPointToRay(touchZero.position);
 				RaycastHit hit ;
 				if (Physics.Raycast (ray, out hit)) {
-					cubeObject.renderer.material.color = new Color( Random.value, Random.value, Random.value, 1.0f );
-					guiText.text = "Last action was: Color change";
+					if( touchZero.tapCount == 1 ) {
+						cubeObject.renderer.material.color = new Color( Random.value, Random.value, Random.value, 1.0f );
+						guiText.text = "Last action was: Color change";
+					}
+				}
+				if( touchZero.tapCount == 2 ) {
+					Defaults();
+					guiText.text = "Last action was: Reset";
 				}
 			}
+
 			if (touchZero.phase == TouchPhase.Moved) {
 				cubeObject.transform.Translate( touchZero.deltaPosition * Time.deltaTime);
 				guiText.text = "Last action was: Cube move";
 			}
-			if( touchZero.tapCount == 2 ) {
-				Defaults();
-				guiText.text = "Last action was: Reset";
-			}
+
 		} else if (Input.touchCount == 2) {
 			Touch touchZero = Input.GetTouch(0);
 			Touch touchOne = Input.GetTouch(1);
@@ -59,12 +65,29 @@ public class CubeBehaviour : MonoBehaviour {
 			
 			// Find the difference in the distances between each frame.
 			float deltaMagnitudeDiff = prevTouchDeltaMag - touchDeltaMag;
-			
+
 			// Otherwise change the field of view based on the change in distance between the touches.
 			camera.fieldOfView += deltaMagnitudeDiff * 0.5f;
 			// Clamp the field of view to make sure it's between 0 and 180.
 			camera.fieldOfView = Mathf.Clamp(camera.fieldOfView, 0.1f, 179.9f);
 			guiText.text = "Last action was: Camera zoom";
+		} else if (Input.touchCount >= 3) {
+			Touch touchZero = Input.GetTouch(0);
+			float rotationValue = 1f * ( Input.touchCount - 2 );
+			guiText.text = "Last action was: Rotate";
+			int seconds = System.DateTime.Now.Second;
+			int timeSplit = 60 / 8; // There are 8 rotation angles..
+
+			//Rotate in different directions depending on time..
+			if( seconds < timeSplit * 1 || seconds < timeSplit * 5 ) {
+				cubeObject.transform.Rotate( rotationValue, rotationValue, 0 );
+			} else if( seconds < timeSplit * 2 || seconds < timeSplit * 6 ) {
+				cubeObject.transform.Rotate( -rotationValue, rotationValue, 0 );
+			} else if( seconds < timeSplit * 3 || seconds < timeSplit * 7 ) {
+				cubeObject.transform.Rotate( -rotationValue, -rotationValue, 0 );
+			} else if( seconds < timeSplit * 4 || seconds < timeSplit * 8 ) {
+				cubeObject.transform.Rotate( rotationValue, -rotationValue, 0 );
+			}
 		}
 	}
 }
